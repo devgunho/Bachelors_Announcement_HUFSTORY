861910e376c9ad33b8c3fe98bff1ed6ac5af6090
gunho <parkgh3737@naver.com>
2019-09-15 14:17:04 +0900

    commit

---
+++ backend/post/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Bachelor_data(models.Model):
+    number = models.IntegerField()
+    data = models.TextField()
+
+    def __str__(self):
+        return str(self.number)
